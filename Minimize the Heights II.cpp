1ef4eb854683011d81d5bfc7239bb3f8c9c80ea5
Arghadyati Bayen <arghadyatibayen@gmail.com>
2023-09-12 10:45:34 +0530

    Add files via upload

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int k, n;
+    cin >> k >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans = v[n - 1] - v[0];
+    int largest = v[n - 1] - k;
+    int smallest = v[0] + k;
+    int ma, mi;
+    for (int i = 0; i < n; i++)
+    {
+        mi = min(smallest, v[i + 1] - k);
+        ma = max(largest, v[i] + k);
+        if (mi < 0)
+            continue;
+        ans = min(ans, ma - mi);
+    }
+    cout << ans << endl;
+}
